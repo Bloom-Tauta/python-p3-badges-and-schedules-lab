090280346a613f98757fe0d1ff9b80a38ad8188d
Bloom <bloomtauta@gmail.com>
2023-08-30 14:08:14 +0300

    done

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,22 @@
 def badge_maker(name):
-    return None
+    return "Hello, my name is " + name + "."
 
 def batch_badge_creator(names):
-    return None
+    list = []
+    for name in names:
+        list.append("Hello, my name is " + name + ".")
+    return list
 
 def assign_rooms(names):
-    return None
-
+    room = 0
+    list = []
+    for name in names:
+        room += 1
+        list.append("Hello, " + name + "! You'll be assigned to room " + str(room) + "!") 
+    return list
+    
 def printer(names):
-    return None
+    for name in batch_badge_creator(names): 
+        print(name)
+    for assignment in assign_rooms(names):
+        print(assignment)
